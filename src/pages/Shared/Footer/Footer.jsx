bca2dbcca7867558d5fcce7c462c7e6bc3165a5d
simanto789 <hazrat.ali789@gmail.com>
2023-05-02 21:17:49 +0600

    create route for madchef-yard

---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p className='text-center'><small> footer will coming soon</small></p>
+        </div>
+    );
+};
+
+export default Footer;
