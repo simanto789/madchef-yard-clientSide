ec06e524b29b1d8aeea818a98964cb959be925f0
simanto789 <hazrat.ali789@gmail.com>
2023-05-04 21:28:26 +0600

    update the chef card

--- src/pages/Home/ChefCard/ChefCard.jsx
+++ src/pages/Home/ChefCard/ChefCard.jsx
@@ -2,17 +2,24 @@ import React from 'react';
 import { Card, Col } from 'react-bootstrap';
 
 const ChefCard = ({ c }) => {
-    const { id, name, experience, recipes } = c;
+    const { id, name, experience, recipes, likes } = c;
     return (
         <div>
             <Col>
-                <Card className='mb-4'>
-                    <Card.Img variant="top" src="holder.js/100px160" />
+                <Card border="danger" className='mb-4'>
+                    <Card.Img variant="top" src={c.picture} />
                     <Card.Body>
                         <Card.Title>{c.name}</Card.Title>
                         <Card.Text>
-                         experience: {c.experience}
+                         Experience: {c.experience}
                         </Card.Text>
+                        <Card.Text>
+                         No of Recipes: {c.recipes}
+                        </Card.Text>
+                        <Card.Text>
+                         Likes: {c.likes}
+                        </Card.Text>
+                        <button type="button" className="btn btn-outline-danger">View Recipes</button>
                     </Card.Body>
                 </Card>
             </Col>
